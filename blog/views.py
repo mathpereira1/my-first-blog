070e28427cd69aac6e81bf53638d581cf3c7655b
Matheus <matheusps3110@gmail.com>
2021-10-05 14:58:42 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-
 
 def post_list(request):
-    posts = Post.objects.filter(crete_date__lte=timezone.now()).order_by('crete_date') 
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
